e288f99b6e4a351f4033c8d43a4bcb5fc93ccaa8
Frank Thompson <frankthompson@gmail.com>
2025-08-26 16:37:05 -0700

    add league redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/league",
+        destination:
+          "https://docs.google.com/spreadsheets/d/1FwNEMlF1KPdVADiPP539y2a2mDiyHpmoQclALHK9nCA/edit?gid=521253915#gid=521253915",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;

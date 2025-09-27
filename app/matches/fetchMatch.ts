df69b49b81e9a58e30ee5a5a7e1acb0860a3490d
Frank Thompson <frankthompson@gmail.com>
2025-09-26 17:23:28 -0700

    add missing file

---
+++ app/matches/fetchMatch.ts
@@ -0,0 +1,17 @@
+import getSQl from "../getSql";
+import { MATCH_FIELDS, getMatchFromRaw } from "./getMatchFromRaw";
+import { Match } from "./Matches";
+
+export default async function fetchMatch(id: string): Promise<null | Match> {
+  const sql = getSQl();
+  const result = await sql`
+    SELECT
+      ${MATCH_FIELDS}
+    FROM match
+    WHERE
+      id = ${id}`;
+  if (result.length === 0) {
+    return null;
+  }
+  return getMatchFromRaw(result[0]);
+}

4616e80adc3204062f315d5d8af1ba714464884e
Frank Thompson <frankthompson@gmail.com>
2025-10-06 02:00:42 -0700

    add missing files

---
+++ app/cast/[id]/GameInfo.tsx
@@ -0,0 +1,30 @@
+import { Game, GAME_NAMES, GoalName } from "@/app/goals";
+import { Tooltip } from "@mantine/core";
+import React from "react";
+
+type Props = {
+    game: Game;
+    description: null | string;
+    goals: null | ReadonlyArray<GoalName>;
+}
+
+export default function GameInfo({ game, description, goals }: Props) {
+    const gameName = goals != null
+        ? <Tooltip
+            label={goals.map((g, index) =>
+                index === 0
+                    ? <React.Fragment key={g}>{g}</React.Fragment>
+                    : <React.Fragment key={g}><br />{g}</React.Fragment>
+            )}>
+            <u>
+                <strong>
+                    {GAME_NAMES[game]}
+                </strong>
+            </u>
+        </Tooltip>
+        : GAME_NAMES[game];
+    const descriptionText = description != null
+        ? ` (${description})`
+        : '';
+    return <>{gameName}{descriptionText}</>
+}
---
+++ app/cast/[id]/InfoCard.tsx
@@ -0,0 +1,31 @@
+import { Card, Title } from "@mantine/core";
+import { ReactNode } from "react";
+
+type Props = {
+    title: ReactNode;
+    children: ReactNode;
+};
+
+export default function InfoCard({ title, children }: Props) {
+    return (
+        <Card
+            shadow="sm"
+            padding="sm"
+            radius="md"
+            withBorder={true}
+            style={{ height: "300px" }}
+        >
+            <Card.Section inheritPadding={true} withBorder={true} py="sm">
+                <Title order={5}>{title}</Title>
+            </Card.Section>
+            <Card.Section
+                inheritPadding={true}
+                withBorder={true}
+                py="sm"
+                style={{ overflowY: "auto" }}
+            >
+                {children}
+            </Card.Section>
+        </Card >
+    );
+}

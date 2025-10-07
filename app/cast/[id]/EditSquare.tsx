71a3a5e4b76921201951be460dc40931bb2d293b
Frank Thompson <frankthompson@gmail.com>
2025-10-07 12:11:42 -0700

    add missing file

---
+++ app/cast/[id]/EditSquare.tsx
@@ -0,0 +1,81 @@
+import { Alert, Button, Drawer, Stack } from "@mantine/core";
+import ColorSelector from "./ColorSelector";
+import { useState } from "react";
+import { BingosyncColor, TBoard } from "@/app/matches/parseBingosyncData";
+import changeColor from "./changeColor";
+
+type Props = {
+  id: string;
+  board: TBoard;
+  editingIndex: number;
+  setEditingIndex: (newEditingIndex: null | number) => unknown;
+};
+
+export default function EditSquare({
+  id,
+  board,
+  editingIndex,
+  setEditingIndex,
+}: Props) {
+  const square = board[editingIndex];
+  const [newColor, setNewColor] = useState<null | BingosyncColor>(null);
+  const [isSaving, setIsSaving] = useState(false);
+
+  const isClearing = square.color !== "blank";
+
+  return (
+    <Drawer.Root
+      position="right"
+      opened={true}
+      onClose={() => setEditingIndex(null)}
+      size={300}
+    >
+      <Drawer.Overlay backgroundOpacity={0} />
+      <Drawer.Content style={{ boxShadow: "-0.2em 0 0.4em black" }}>
+        <Drawer.Header>
+          <Drawer.Title>{isClearing ? "Clear" : "Assign"} Square</Drawer.Title>
+          <Drawer.CloseButton />
+        </Drawer.Header>
+        <Drawer.Body>
+          <Stack>
+            <Alert
+              color="red"
+              title="Please be absolutely certain you need to manually change this square. If you aren't sure, carefully review the stream VOD or ask the players to pause."
+            />
+            <span>
+              <strong>{square.name}</strong>
+            </span>
+            {!isClearing && (
+              <ColorSelector
+                label="New color"
+                color={newColor}
+                setColor={setNewColor}
+              />
+            )}
+            <Button
+              disabled={isSaving || (!isClearing && newColor == null)}
+              onClick={async () => {
+                setIsSaving(true);
+                const color = isClearing ? square.color : nullthrows(newColor);
+                try {
+                  await changeColor(id, editingIndex, color, isClearing);
+                } finally {
+                  setIsSaving(false);
+                }
+              }}
+            >
+              Confirm {isClearing ? "Clear" : "Assignment"}
+            </Button>
+          </Stack>
+        </Drawer.Body>
+      </Drawer.Content>
+    </Drawer.Root>
+  );
+}
+
+function nullthrows(color: null | BingosyncColor): BingosyncColor {
+  if (color == null) {
+    throw new Error("Must select a color before confirming!");
+  }
+  return color;
+}
---
+++ app/cast/[id]/changeColor.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import { BingosyncColor } from "@/app/matches/parseBingosyncData";
+import getPersonalSessionCookie from "./getPersonalSessionCookie";
+
+export default async function changeColor(
+  id: string,
+  slot: number,
+  color: BingosyncColor,
+  removeColor: boolean
+): Promise<void> {
+  const cookie = await getPersonalSessionCookie();
+  if (cookie == null) {
+    throw new Error(
+      "Failed to find sessionid cookie! Please refresh the page."
+    );
+  }
+
+  await fetch("https://www.bingosync.com/api/select", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Cookie: cookie,
+    },
+    body: JSON.stringify({
+      room: id,
+      slot: (slot + 1).toString(),
+      color,
+      remove_color: removeColor,
+    }),
+  });
+}

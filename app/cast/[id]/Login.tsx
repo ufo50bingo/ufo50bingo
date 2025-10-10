bd7938e24bb67488fba34b7f125d58be63928cd7
Frank Thompson <frankthompson@gmail.com>
2025-10-10 12:16:13 -0700

    fix up intro text

--- app/cast/[id]/Login.tsx
+++ app/cast/[id]/Login.tsx
@@ -34,22 +34,53 @@ export default function Login() {
             <Title order={1}>Cast a Match</Title>
             <span>
               <strong>
-                If you are a player, please go to the{' '}
-                <Link href={`https://www.bingosync.com/room/${id}`}>official Bingosync room</Link>{' '}
+                If you are a player, please go to the{" "}
+                <Link href={`https://www.bingosync.com/room/${id}`}>
+                  official Bingosync room
+                </Link>{" "}
                 instead!
               </strong>
             </span>
             <span>Use this page to cast a match! Features include:</span>
             <List>
-              <ListItem>The live board view can also display difficulty tags on squares.</ListItem>
-              <ListItem>Player score are computed automatically and displayed next to the board.</ListItem>
-              <ListItem>The full-featured general goad tracker also displays progress next to the board.</ListItem>
-              <ListItem>General goals display a list of all relevant games, sorted by rough time estimates.</ListItem>
-              <ListItem>Possible synergies are underlined, and hovering shows the relevant other goals.</ListItem>
-              <ListItem>A countdown button can automatically count down to board reveal and match start in chat.</ListItem>
-              <ListItem>Casters can clear or grant squares to players without needing to log in on a separate window. Just click on a square!</ListItem>
-              <ListItem>Leaderboard thresholds and gift requirements are displayed for every game.</ListItem>
-              <ListItem>The "Create new board" button allows creating a new board without having to paste anything.</ListItem>
+              <ListItem>
+                The live board view displays difficulty tags on squares.
+              </ListItem>
+              <ListItem>
+                Player score are computed automatically and displayed next to
+                the board, with the tiebreaker winner underlined.
+              </ListItem>
+              <ListItem>
+                General goal progress has per-game tracking for each player.
+                Progress is synced between all casters and displayed next to the
+                board.
+              </ListItem>
+              <ListItem>
+                General goals display a list of all relevant games, with fastest
+                games first by default. Alphabetical and chronological sorting
+                options are available also.
+              </ListItem>
+              <ListItem>
+                Possible synergies are underlined, and hovering shows the
+                relevant other goals and their positions on the board in [row,
+                column] format.
+              </ListItem>
+              <ListItem>
+                A countdown button can automatically count down to board reveal
+                and match start in chat.
+              </ListItem>
+              <ListItem>
+                Casters can clear or grant squares to players without needing to
+                log in on a separate window. Just click on a square!
+              </ListItem>
+              <ListItem>
+                Leaderboard thresholds and gift requirements are displayed for
+                every game.
+              </ListItem>
+              <ListItem>
+                The "Create new board" button allows creating a new board
+                without having to paste anything.
+              </ListItem>
               <ListItem>Disconnections are detected automatically.</ListItem>
             </List>
           </Stack>

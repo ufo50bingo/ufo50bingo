87604e0f97371ef67b8f5458f15e5225426c347e
Frank Thompson <frankthompson@gmail.com>
2025-10-27 14:48:25 -0700

    track feed

---
+++ app/daily/useAttemptNumber.ts
@@ -0,0 +1,30 @@
+import { useCallback, useState } from "react";
+
+export default function useAttemptNumber(
+  date: string,
+): [number, (newAttempt: number) => unknown] {
+  const key = `daily-attempt-${date}`;
+  const [attempt, setAttemptRaw] = useState<number>(() => {
+    if (global.window == undefined || localStorage == null) {
+      return 1;
+    }
+    const fromStorage = localStorage.getItem(key);
+    if (fromStorage == null || fromStorage === "") {
+      return 1;
+    }
+    return Number(fromStorage);
+  });
+
+  const setAttempt = useCallback(
+    async (newAttempt: number) => {
+      setAttemptRaw(newAttempt);
+      if (global.window == undefined || localStorage == null) {
+        return;
+      }
+      localStorage.setItem(key, newAttempt.toString());
+    },
+    [key]
+  );
+
+  return [attempt, setAttempt];
+}

e601aa4a12f491e209888eb08967f0157e2a39d0
Frank Thompson <frankthompson@gmail.com>
2025-10-27 04:24:01 -0700

    fix cache

--- app/daily/clearDaily.ts
+++ app/daily/clearDaily.ts
@@ -4,7 +4,6 @@ import { revalidateTag } from "next/cache";
 import getSql from "../getSql";
 
 export default async function clearDaily(date: string): Promise<void> {
-  const sql = getSql(false, ['daily']);
+  const sql = getSql(false);
   await sql`DELETE FROM daily WHERE date = ${date};`;
-  revalidateTag("daily");
 }

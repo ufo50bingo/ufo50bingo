2399120a5aeb6c1fb602317784c00e5fd3c2e1ea
Frank Thompson <frankthompson@gmail.com>
2025-10-28 13:47:58 -0700

    daily color selection

--- app/daily/Daily.tsx
+++ app/daily/Daily.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { TBoard } from "../matches/parseBingosyncData";
 import Board from "../Board";
 import { Container, Card, Text, Button, Stack, Title } from "@mantine/core";
 import useTimer from "../useTimer";
 import { LocalDate, toISODate } from "./localDate";
+import useDailyColor from "./useDailyColor";
+import ColorSelector from "../room/[id]/common/ColorSelector";
 
 type Props = {
     date: LocalDate;
@@ -14,9 +16,12 @@ type Props = {
 
 export default function Daily({ date, board: plainBoard }: Props) {
     const [isHidden, setIsHidden] = useState(true);
-    const [board, setBoard] = useState<TBoard>(() =>
+    const [color, setColor] = useDailyColor();
+    const [completedIndexes, setCompletedIndexes] = useState<Set<number>>(new Set());
+    const board = useMemo<TBoard>(() =>
         plainBoard
-            .map(name => ({ name, color: "blank" }))
+            .map((name, index) => ({ name, color: completedIndexes.has(index) ? color : "blank" })),
+        [plainBoard, completedIndexes, color],
     );
 
     const { isRunning, pause, start, timer } = useTimer({
@@ -25,42 +30,56 @@ export default function Daily({ date, board: plainBoard }: Props) {
     });
 
     return (
-        <Container>
+        <Container my="md">
             <Card shadow="sm" padding="sm" radius="md" withBorder>
-                <Stack gap={4} style={{ alignSelf: "center" }}>
-                    <Title order={1}>Daily Bingo — ({toISODate(date)})</Title>
-                    <Board
-                        board={board}
-                        onClickSquare={(squareIndex: number) => {
-                            const newBoard = [...board];
-                            const newSquare = { ...board[squareIndex] };
-                            newSquare.color = newSquare.color === "blank" ? "red" : "blank";
-                            newBoard[squareIndex] = newSquare;
-                            setBoard(newBoard);
-                        }}
-                        hiddenText={
-                            <>
-                                <div>Click to Reveal</div>
-                                <div>Start playing when the timer hits 0:00.0!</div>
-                            </>
-                        }
-                        isHidden={isHidden}
-                        setIsHidden={() => {
-                            start();
-                            setIsHidden(false);
-                        }}
-                        shownDifficulties={[]}
-                    />
-                    <Text style={{ alignSelf: "center" }} size="xl">
-                        {timer}
-                    </Text>
-                    <Button
-                        onClick={() => (isRunning ? pause() : start())}
-                        fullWidth={true}
-                    >
-                        {isRunning ? "Pause" : "Resume"}
-                    </Button>
-                </Stack>
+                <Card.Section withBorder={true} inheritPadding={true} py="xs">
+                    <Stack>
+                        <Title order={1}>Daily Bingo — {date.month}/{date.day}</Title>
+                        <Text>
+                            Your goal is to claim a bingo as fast as possible.<br />
+                            After you've claimed a bingo, you can optionally continue to claim 13 squares,
+                            and then continue to a blackout!
+                        </Text>
+                        <ColorSelector label="Select your preferred color" color={color} setColor={setColor} />
+                    </Stack>
+                </Card.Section>
+                <Card.Section withBorder={true} inheritPadding={true} py="xs">
+                    <Stack gap={4} style={{ alignSelf: "center" }}>
+                        <Board
+                            board={board}
+                            onClickSquare={(squareIndex: number) => {
+                                const newSet = new Set(completedIndexes);
+                                if (newSet.has(squareIndex)) {
+                                    newSet.delete(squareIndex);
+                                } else {
+                                    newSet.add(squareIndex);
+                                }
+                                setCompletedIndexes(newSet);
+                            }}
+                            hiddenText={
+                                <>
+                                    <div>Click to reveal today's board.</div>
+                                    <div>Start playing when the timer hits 0:00.0!</div>
+                                </>
+                            }
+                            isHidden={isHidden}
+                            setIsHidden={() => {
+                                start();
+                                setIsHidden(false);
+                            }}
+                            shownDifficulties={[]}
+                        />
+                        <Text style={{ alignSelf: "center", fontVariantNumeric: "tabular-nums" }} size="xl">
+                            {timer}
+                        </Text>
+                        <Button
+                            onClick={() => (isRunning ? pause() : start())}
+                            fullWidth={true}
+                        >
+                            {isRunning ? "Pause" : "Resume"}
+                        </Button>
+                    </Stack>
+                </Card.Section>
             </Card>
         </Container>
     );
---
+++ app/daily/useDailyColor.ts
@@ -0,0 +1,31 @@
+import { BingosyncColor } from "@/app/matches/parseBingosyncData";
+import { useCallback, useState } from "react";
+
+const KEY = "daily-color";
+
+export default function useDailyColor(
+): [BingosyncColor, (newColor: BingosyncColor) => unknown] {
+  const [color, setColorRaw] = useState<BingosyncColor>(() => {
+    if (global.window == undefined || localStorage == null) {
+      return "red";
+    }
+    const fromStorage = localStorage.getItem(KEY);
+    if (fromStorage == null || fromStorage === "") {
+      return "red";
+    }
+    return fromStorage as BingosyncColor;
+  });
+
+  const setColor = useCallback(
+    async (newColor: BingosyncColor) => {
+      setColorRaw(newColor);
+      if (global.window == undefined || localStorage == null) {
+        return;
+      }
+      localStorage.setItem(KEY, newColor);
+    },
+    []
+  );
+
+  return [color, setColor];
+}

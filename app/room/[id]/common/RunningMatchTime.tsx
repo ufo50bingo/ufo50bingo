3810a9fa4b3e206febf56020127a848bed4b8503
Frank Thompson <frankthompson@gmail.com>
2025-10-28 13:47:58 -0700

    update timer state and local storage

--- app/room/[id]/common/RunningMatchTime.tsx
+++ app/room/[id]/common/RunningMatchTime.tsx
@@ -2,25 +2,21 @@ import Duration from "@/app/practice/Duration";
 import { useEffect, useState } from "react";
 
 type Props = {
-    curEndTime: number;
-    matchTime: number;
+  curEndTime: number;
+  matchTime: number;
 };
 
-export default function RunningMatchTime({
-    curEndTime,
-    matchTime,
-}: Props) {
-    const [_dummyState, setDummyState] = useState(0);
-    useEffect(() => {
-        const interval = setInterval(
-            () => setDummyState((prevDummyState) => prevDummyState + 1),
-            1000
-        );
-        return () => clearInterval(interval);
-    }, []);
-    const totalRemaining = Math.max(0, curEndTime - Date.now());
-    const timeToDisplay = totalRemaining > matchTime ? totalRemaining - matchTime : totalRemaining;
-    return (
-        <Duration duration={timeToDisplay} showDecimal={false} />
+export default function RunningMatchTime({ curEndTime, matchTime }: Props) {
+  const [_dummyState, setDummyState] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(
+      () => setDummyState((prevDummyState) => prevDummyState + 1),
+      1000
     );
+    return () => clearInterval(interval);
+  }, []);
+  const totalRemaining = curEndTime - Date.now();
+  const timeToDisplay =
+    totalRemaining > matchTime ? totalRemaining - matchTime : totalRemaining;
+  return <Duration duration={timeToDisplay} showDecimal={false} />;
 }

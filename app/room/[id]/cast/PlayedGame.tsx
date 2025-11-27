bd235308761007457eb7c7cf44f2b1ccdebbd289
Frank Thompson <frankthompson@gmail.com>
2025-11-27 09:33:13 -0800

    add time to PlayedGame

--- app/room/[id]/cast/PlayedGame.tsx
+++ app/room/[id]/cast/PlayedGame.tsx
@@ -1,19 +1,29 @@
 import { ProperGame } from "@/app/goals";
+import classes from "./PlayedGame.module.css";
+import RunningDuration from "@/app/practice/RunningDuration";
+import Duration from "@/app/practice/Duration";
 
 type Props = {
   game: ProperGame;
+  startTime: number;
+  endTime: null | undefined | number;
 };
 
-export default function PlayedGame({ game }: Props) {
+export default function PlayedGame({ game, startTime, endTime }: Props) {
   return (
-    <img
-      src={`/games/${game}.png`}
-      style={{
-        width: "100%",
-        height: "100%",
-        objectFit: "contain",
-        imageRendering: "pixelated",
-      }}
-    />
+    <div className={classes.container}>
+      <img src={`/games/${game}.png`} className={classes.gameIcon} />
+      <div className={classes.tag}>
+        {endTime == null ? (
+          <RunningDuration
+            curStartTime={startTime}
+            accumulatedDuration={0}
+            showDecimal={false}
+          />
+        ) : (
+          <Duration duration={endTime - startTime} showDecimal={false} />
+        )}
+      </div>
+    </div>
   );
 }

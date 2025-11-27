5db121dc884eeda5e03df941c4a8ec461fa0125d
Frank Thompson <frankthompson@gmail.com>
2025-11-27 09:33:13 -0800

    RecentGames filled in

---
+++ app/room/[id]/cast/RecentGames.tsx
@@ -0,0 +1,28 @@
+import PlayedGame from "./PlayedGame";
+import { CurrentGame } from "./useSyncedState";
+
+type Props = {
+    recentGames: ReadonlyArray<CurrentGame>;
+    limit: number;
+};
+
+export default function RecentGames({ recentGames, limit }: Props) {
+    const entries = recentGames.map((recentGame, index) => {
+        const { game, start_time: startTime } = recentGame;
+        if (game == null) {
+            return null;
+        }
+        const endTime = index > 0
+            ? recentGames[index - 1].start_time
+            : null;
+        return (
+            <PlayedGame
+                key={startTime}
+                game={game}
+                startTime={startTime}
+                endTime={endTime}
+            />
+        );
+    }).slice(0, limit);
+    return <>{entries}</>;
+}

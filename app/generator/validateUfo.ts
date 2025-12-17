78d06b451d932208df016d8659d65d32dbd1d21b
Frank Thompson <frankthompson@gmail.com>
2025-12-17 14:14:18 -0800

    tiny bit of validation cleanup

--- app/generator/validateUfo.ts
+++ app/generator/validateUfo.ts
@@ -34,22 +34,22 @@ export default function validateUfo(json: string): Return {
       const maxCount = maxTokenCounts[token];
       const options = pasta.tokens[token];
       if (options == null) {
-        errors.push(`Token "${token}" appears in a goal but does not have options specified in the "tokens" object.`);
+        errors.push(`✖ Token "${token}" appears in a goal but does not have options specified in the "tokens" object.`);
       } else if (options.length < maxCount) {
         const allOptions = options.map(option => `"${option}"`).join(", ");
-        errors.push(`Token "${token}" appears in a goal ${maxCount} times, but only has ${options.length} options in its definition in the "tokens" object [${allOptions}]`);
+        errors.push(`✖ Token "${token}" appears in a goal ${maxCount} times, but only has ${options.length} options in its definition in the "tokens" object [${allOptions}]`);
       }
     });
     // are there any unused tokens?
     Object.keys(pasta.tokens).forEach(token => {
       if (maxTokenCounts[token] == null) {
-        warnings.push(`Token "${token}" is defined in the "tokens" object but is not used in any goals`);
+        warnings.push(`⚠ Token "${token}" is defined in the "tokens" object but is not used in any goals`);
       }
     });
     // do categories add up propery?
     const categorySum = Object.values(pasta.category_counts).reduce((acc, count) => acc + count);
     if (categorySum !== 25) {
-      errors.push(`category_counts sum must be 25, but got ${categorySum}`);
+      errors.push(`✖ category_counts sum must be 25, but got ${categorySum}`);
     }
     // are all categories actually in the goals list?
     Object.keys(pasta.category_counts).forEach(category => {
@@ -59,31 +59,32 @@ export default function validateUfo(json: string): Return {
       }
       const goals = pasta.goals[category];
       if (goals == null) {
-        errors.push(`Category "${category}" has count ${count} but does not appear in the "goals" object`);
+        errors.push(`✖ Category "${category}" has count ${count} but does not appear in the "goals" object`);
+        return;
       }
       const numGoals = Object.values(goals).reduce((acc, goalList) => acc + goalList.length, 0);
       if (numGoals < count) {
-        errors.push(`Category "${category}" has count ${count} but only has ${numGoals} goals in the "goals" object`);
+        errors.push(`✖ Category "${category}" has count ${count} but only has ${numGoals} goals in the "goals" object`);
       }
     });
     // are any categories missing from category_counts?
     Object.keys(pasta.goals).forEach(category => {
       if (pasta.category_counts[category] == null) {
-        warnings.push(`Category "${category}" appears in the "goals" object but not in the "category_counts" object`);
+        warnings.push(`⚠ Category "${category}" appears in the "goals" object but not in the "category_counts" object`);
       }
     });
     // are any categories present in category_difficulty_tiers but not category_counts?
     (pasta.category_difficulty_tiers ?? []).forEach(tier => {
       tier.forEach(category => {
         if (pasta.category_counts[category] == null) {
-          warnings.push(`Category "${category}" appears in the "category_difficulty_tiers" object but not the "category_counts" object`);
+          warnings.push(`⚠ Category "${category}" appears in the "category_difficulty_tiers" object but not the "category_counts" object`);
         }
       });
     });
     // are any categories present in categories_with_global_group_repeat_prevention but not category_counts?
     (pasta.categories_with_global_group_repeat_prevention ?? []).forEach(category => {
       if (pasta.category_counts[category] == null) {
-        warnings.push(`Category "${category}" appears in the "categories_with_global_group_repeat_prevention" object but not the "category_counts" object`);
+        warnings.push(`⚠ Category "${category}" appears in the "categories_with_global_group_repeat_prevention" object but not the "category_counts" object`);
       }
     });
     return errors.length > 0

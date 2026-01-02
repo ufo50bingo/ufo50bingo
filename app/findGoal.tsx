5187b69aa6591d021292aa8e2b5ef8b50fac818b
Frank Thompson <frankthompson@gmail.com>
2026-01-02 10:42:52 -0800

    maybe done??

--- app/findGoal.tsx
+++ app/findGoal.tsx
@@ -2,67 +2,99 @@
 
 import { UFOPasta } from "./generator/ufoGenerator";
 
+const CACHE: Array<[UFOPasta, ProcessedPasta]> = [];
+
 type FoundGoal = {
   goal: string;
   category: string;
   subcategory: string;
-  tokens: { [tokenName: string]: ReadonlyArray<string> };
+  tokens: ReadonlyArray<string>;
+};
+type Tags = { category: string; subcategory: string };
+type Plain = { [goal: string]: Tags };
+interface WithTokenNoTags {
+  regex: RegExp;
+  goal: string;
+  tokens: ReadonlyArray<string>;
+}
+interface WithToken extends WithTokenNoTags {
+  tags: Tags;
+}
+type WithTokens = Array<WithToken>;
+type ProcessedPasta = {
+  plain: Plain;
+  withTokens: WithTokens;
 };
 
 export default function findGoal(
   goal: string,
   pasta: UFOPasta
 ): null | FoundGoal {
+  let processed = CACHE.find((item) => item[0] === pasta)?.[1];
+  if (processed == null) {
+    processed = preprocess(pasta);
+    CACHE.push([pasta, processed]);
+  }
+  const plainResult = processed.plain[goal];
+  if (plainResult != null) {
+    return {
+      goal,
+      category: plainResult.category,
+      subcategory: plainResult.subcategory,
+      tokens: [],
+    };
+  }
+  for (const option of processed.withTokens) {
+    const match = goal.match(option.regex);
+    if (match != null) {
+      return {
+        goal,
+        category: option.tags.category,
+        subcategory: option.tags.subcategory,
+        tokens: match.slice(1),
+      };
+    }
+  }
   return null;
 }
 
 const TOKEN_REGEX = /\{\{([^{}]*)\}\}/g;
-
-type Tags = { category: string, subcategory: string };
-type Plain = { [goal: string]: Tags };
-interface WithTokenNoTags {
-  regex: RegExp,
+function preprocessGoalWithToken(
   goal: string,
-  tokens: ReadonlyArray<string>,
-};
-interface WithToken extends WithTokenNoTags {
-  tags: Tags,
-}
-type WithTokens = Array<WithToken>;
-
-function preprocessGoalWithToken(goal: string, pasta: UFOPasta): WithTokenNoTags {
+  pasta: UFOPasta
+): WithTokenNoTags {
   const matches = goal.matchAll(TOKEN_REGEX);
   // token locations should all be disjoint since they cannot be nested
-  let regexStr = '';
+  let regexStr = "";
   let startIndex = 0;
   const tokens: Array<string> = [];
   for (const match of matches) {
-    // @ts-ignore RegExp.escape exists on all modern browsers
+    // @ts-expect-error RegExp.escape exists on all modern browsers
     regexStr += RegExp.escape(goal.slice(startIndex, match.index));
     startIndex = match.index + match[0].length;
     tokens.push(match[1]);
     const tokenOptions = pasta.tokens[match[1]];
-    const tokenRegex = tokenOptions.map(option =>
-      // @ts-ignore RegExp.escape exists on all modern browsers
-      RegExp.escape(option)
-    ).join('|');
-    regexStr += '(' + tokenRegex + ')';
+    const tokenRegex = tokenOptions
+      .map((option) =>
+        // @ts-expect-error RegExp.escape exists on all modern browsers
+        RegExp.escape(option)
+      )
+      .join("|");
+    regexStr += "(" + tokenRegex + ")";
   }
-  // @ts-ignore RegExp.escape exists on all modern browsers
+  // @ts-expect-error RegExp.escape exists on all modern browsers
   regexStr += RegExp.escape(goal.slice(startIndex, goal.length));
 
-  console.log(regexStr);
   return {
     regex: RegExp(regexStr),
     goal,
-    tokens
+    tokens,
   };
 }
 
-function preprocess(pasta: UFOPasta): [Plain, WithTokens] {
+function preprocess(pasta: UFOPasta): ProcessedPasta {
   const plain: Plain = {};
   const withTokens: WithTokens = [];
-  const tokens = pasta.tokens;
   Object.keys(pasta.goals).forEach((category) => {
     Object.keys(pasta.goals[category]).forEach((subcategory) => {
       pasta.goals[category][subcategory].forEach((goal) => {
@@ -76,5 +108,5 @@ function preprocess(pasta: UFOPasta): [Plain, WithTokens] {
       });
     });
   });
-  return [plain, withTokens];
+  return { plain, withTokens };
 }

abcb3fc45b5fd6c3dddffc7662913f6be72cf04e
Frank Thompson <frankthompson@gmail.com>
2026-02-13 08:45:27 -0800

    simple generals

--- app/room/[id]/play/SimpleGeneralTracker.tsx
+++ app/room/[id]/play/SimpleGeneralTracker.tsx
@@ -55,7 +55,7 @@ export default function SimpleGeneralTracker({
                     ...state,
                     [item.foundGoal.resolvedGoal]: Math.max(
                       (state[item.foundGoal.resolvedGoal] ?? 0) - 1,
-                      0
+                      0,
                     ),
                   });
                 }}
@@ -91,70 +91,59 @@ export default function SimpleGeneralTracker({
 
 function getAbbreviatedName(goal: FoundStandardGeneral): string {
   switch (goal.goal) {
-    case "Collect {{cherry_count}} cherry disks from games on this card":
-      return `Cherry (${goal.tokens[0]})`;
-    case "Collect {{gold_count}} gold disks from games on this card":
-      return `Gold (${goal.tokens[0]})`;
+    // GIFT
     case "Collect {{gift_count}} gifts from games on this card":
       return `Gifts (${goal.tokens[0]})`;
-    case "ARCADE ACE: Gold Disk any 3 of the 16 “ARCADE” games":
-      return "Arcade (3)";
-    case "TRIATHLON: Gold Disk any 3 of the 5 “SPORT” games":
-      return "Sport (3)";
-    case "Collect a beverage in 6 games":
-      return "Beverage (6)";
-    case "Collect a food item in 8 games":
-      return "Food (8)";
-    case "Beat 2 levels in 8 different games":
-      return "2 levels (8)";
-    case "Beat 4 levels in 5 different games":
+    // GOLD/CHERRY
+    case "Cherry disk {{cherry_count}} games on this card":
+      return `Cherry (${goal.tokens[0]})`;
+    case "Gold disk {{gold_count}} games on this card":
+      return `Gold (${goal.tokens[0]})`;
+    // BOSS/LEVEL
+    case "Beat 2 levels in 6 games on this card":
+      return "2 levels (6)";
+    case "Beat 4 levels in 5 games on this card":
       return "4 levels (5)";
-    case "Beat 8 levels in 3 different games":
+    case "Beat 8 levels in 3 games on this card":
       return "8 levels (3)";
-    case "Collect a key in 7 games":
-      return "Key (7)";
+    case "Defeat 2 bosses in 3 games on this card":
+      return "2 bosses (3)";
+    case "Defeat 7 bosses from games on this card":
+      return "Bosses (7)";
+    case "Defeat a boss in 5 games on this card":
+      return "Boss (5)";
+    // COLLECTATHON
+    case "Collect 2 keys in 5 games":
+      return "2 keys (5)";
     case "Open 2 chests in 5 games":
       return "2 chests (5)";
-    case "Buy an item from a shop in 10 games":
-      return "Shop (10)";
-    case "Find an easter egg UFO in 5 games":
-      return "UFO (5)";
-    case "Earn an extra life/1UP in 8 games":
-      return "1UP (8)";
-    case "Find an egg in 10 games":
-      return "Egg (10)";
-    case "Increase your base HP in 6 games":
-      return "HP (6)";
-    case "Defeat 2 bosses in 4 different games":
-      return "2 bosses (4)";
-    case "Defeat 7 bosses":
-      return "Bosses (7)";
-    case "Defeat a boss in 6 different games":
-      return "Bosses (6)";
-    // case "Enter a top 3 score on 2 arcade leaderboards":
-    // case "Enter a top 3 score on 3 arcade leaderboards":
-    case "Enter a top 5 score on 4 arcade leaderboards":
+    case "Buy an item from 2 unique shops in one run in 6 games":
+      return "2 shops (6)";
+    case "Earn 2 extra lives/1-Ups in 5 games":
+      return "2 extra lives (5)";
+    case "Increase your base HP twice in 4 games":
+      return "2 HP (4)";
+    case "Surpass the top 5 score from 4 arcade leaderboards":
       return "Top 5 (4)";
-    case "PILOT PARTY: Collect 4 gifts: Campanella 1/2/3, Planet Zoldath, Pilot Quest, The Big Bell Race":
-      return "Pilot Party (4)";
-    case "ALPHA TRILOGY: Gold Velgress, Overbold, and Quibble Race as Alpha":
-      return "Alpha (3)";
-    case "AMY: Playing as Amy, beat 1 level in Party House, 2 in Hot Foot, 2 in Fist Hell":
-      return "Amy (3)";
-    case "CAMPANELLA TRILOGY: Beat two worlds in Campanella, two in Campanella 2, one in Campanella 3":
-      return "Campanella (3)";
-    case "DAY JOB: Beat 1 level in Bug Hunter, 2 in Onion Delivery, 3 in Rail Heist":
-      return "Day Job (3)";
-    case "METROIDVANIA: Collect Abilities: 3 in Porgy, 2 in Vainger, 1 in Golfaria":
-      return "Metroidvania (3)";
-    case "PUZZLER: Beat 5 levels in Block Koala, Camouflage, Warptank":
-      return "Puzzler (3)";
-    case "RACER: Win 4 races in Paint Chase, The Big Bell Race, Quibble Race":
-      return "Racer (3)";
-    case "ROLE-PLAYER: Level up all your characters twice in Divers, Valbrace, Grimstone":
-      return "Role-player (3)";
-    case "WAR IS BAD: Win 3 battles in Attactics, Avianos, Combatants":
-      return "War is Bad (3)";
+    case "Defeat 6 different enemy types in 6 games":
+      return "6 enemy types (6)";
+    // THEME
+    case "CAMPANELLA TRILOGY: Beat 5 total worlds across Campanella 1, 2, and 3":
+      return "Camp Trilogy (5 worlds)";
+    case "SHOOTER: Beat 5 waves/stages across Elfazar's Hat, Seaside Drive, and Caramel Caramel":
+      return "Shooter (5 waves/stages)";
+    case "DAY JOB: Beat 9 levels across Rail Heist, Onion Delivery, and Bug Hunter":
+      return "Day Job (9 levels)";
+    case "RACER: Win 12 races across Paint Chase, The Big Bell Race, and Quibble Race":
+      return "Racer (12 races)";
+    case "PUZZLER: Beat 15 levels across Block Koala, Devilition, and Warptank":
+      return "Puzzler (15 levels)";
+    case "AMY: Beat 5 levels across Party House, Fist Hell, and Hot Foot":
+      return "Amy (5 levels)";
+    case "WAR IS BAD: Win 9 battles across Attactics, Avianos, and Combatants with 2+ in each":
+      return "War Is Bad (9 battles)";
+    case "METROIDVANIA: Collect 6 abilities across Porgy, Vainger, and Golfaria":
+      return "Metroidvania (6 abilities)";
     default:
       return goal.resolvedGoal;
   }

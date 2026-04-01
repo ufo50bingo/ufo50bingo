e29e93991f69ea77cb0e6ac87882c7269fb2b37f
Frank Thompson <frankthompson@gmail.com>
2026-03-31 21:34:26 -0700

    finish? sync all back end

--- app/matches/syncAllToGsheet.ts
+++ app/matches/syncAllToGsheet.ts
@@ -1,6 +1,9 @@
 "use server";
 
+import { google } from "googleapis";
 import getSql from "../getSql";
+import getDataSheetId from "./getDataSheetId";
+import getGsheetSyncData from "./getGsheetSyncData";
 import { getMatchFromRaw, MATCH_FIELDS } from "./getMatchFromRaw";
 
 export default async function syncAllToGsheet(
@@ -16,5 +19,23 @@ export default async function syncAllToGsheet(
         ${MATCH_FIELDS}
       FROM match
       WHERE${seasonFilter}`;
-  return getMatchFromRaw(result[0]);
+  const matches = result.map(r => getMatchFromRaw(r));
+
+  const allRows = matches.map(match => getGsheetSyncData(match)).filter(result => result != null).flat();
+
+  const auth = new google.auth.JWT({
+    email: process.env.GSHEETS_ACCOUNT_EMAIL,
+    key: process.env.GSHEETS_ACCOUNT_PRIVATE_KEY,
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  });
+  const sheet = google.sheets("v4");
+  await sheet.spreadsheets.values.append({
+    spreadsheetId: getDataSheetId(season),
+    auth: auth,
+    range: "Data",
+    valueInputOption: "RAW",
+    requestBody: {
+      values: allRows,
+    },
+  });
 }

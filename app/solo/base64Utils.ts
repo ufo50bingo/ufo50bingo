d74f21ce563e12c534a2a0e5a660a5e97969013f
Frank Thompson <frankthompson@gmail.com>
2026-04-21 10:09:42 -0700

    starting solo board page

---
+++ app/solo/base64Utils.ts
@@ -0,0 +1,31 @@
+function base64ToBytes(base64: string): Uint8Array<ArrayBuffer> {
+  const binString = atob(base64);
+  return Uint8Array.from(binString, (v, _) => v.codePointAt(0)!);
+}
+
+function bytesToBase64(bytes: Uint8Array<ArrayBuffer>) {
+  const binString = Array.from(bytes, (byte) =>
+    String.fromCodePoint(byte),
+  ).join("");
+  return btoa(binString);
+}
+
+function base64UrlEncode(str: string): string {
+  return bytesToBase64(new TextEncoder().encode(str))
+    .replace(/\//g, "_")
+    .replace(/\+/g, "-");
+}
+
+function base64UrlDecode(str: string): string {
+  return new TextDecoder().decode(
+    base64ToBytes(str.replace(/_/g, "/").replace(/-/g, "+")),
+  );
+}
+
+export function encodeGoalList(goalList: ReadonlyArray<string>): string {
+  return base64UrlEncode(goalList.join("\n"));
+}
+
+export function decodeGoalList(encoded: string): ReadonlyArray<string> {
+  return base64UrlDecode(encoded).split("\n");
+}

939c6c61eee10d6bde4b8d6a9bc20cf3ec46cb60
Frank Thompson <frankthompson@gmail.com>
2026-04-21 14:30:51 -0700

    brotli maybe done?

--- app/solo/base64Utils.ts
+++ app/solo/base64Utils.ts
@@ -1,38 +1,46 @@
 import brotliPromise from "brotli-wasm"; // Import the default export
 
-function base64ToBytes(base64: string): Uint8Array<ArrayBuffer> {
+function base64ToBytes(base64: string): Uint8Array {
   const binString = atob(base64);
   return Uint8Array.from(binString, (v, _) => v.codePointAt(0)!);
 }
 
-function bytesToBase64(bytes: Uint8Array<ArrayBuffer>) {
+function bytesToBase64(bytes: Uint8Array) {
   const binString = Array.from(bytes, (byte) =>
     String.fromCodePoint(byte),
   ).join("");
   return btoa(binString);
 }
 
-function base64UrlEncode(str: string): string {
-  return bytesToBase64(new TextEncoder().encode(str))
-    .replace(/\//g, "_")
-    .replace(/\+/g, "-");
+function toUrlSafe(base64: string): string {
+  return base64.replace(/\//g, "_").replace(/\+/g, "-");
 }
 
-async function compress(str: string): Promise<string> {
+function fromUrlSafe(urlSafe: string): string {
+  return urlSafe.replace(/_/g, "/").replace(/-/g, "+");
+}
+
+async function compress(str: string): Promise<Uint8Array> {
   const brotli = await brotliPromise;
   return brotli.compress(new TextEncoder().encode(str));
 }
 
-function base64UrlDecode(str: string): string {
-  return new TextDecoder().decode(
-    base64ToBytes(str.replace(/_/g, "/").replace(/-/g, "+")),
-  );
+async function decompress(encoded: Uint8Array): Promise<string> {
+  const brotli = await brotliPromise;
+  return new TextDecoder().decode(brotli.decompress(encoded));
 }
 
-export function encodeGoalList(goalList: ReadonlyArray<string>): string {
-  return base64UrlEncode(goalList.join("\n"));
+export async function encodeGoalList(
+  goalList: ReadonlyArray<string>,
+): Promise<string> {
+  const compressed = await compress(goalList.join("\n"));
+  return toUrlSafe(bytesToBase64(compressed));
 }
 
-export function decodeGoalList(encoded: string): ReadonlyArray<string> {
-  return base64UrlDecode(encoded).split("\n");
+export async function decodeGoalList(
+  encoded: string,
+): Promise<ReadonlyArray<string>> {
+  const compressed = base64ToBytes(fromUrlSafe(encoded));
+  const joinedList = await decompress(compressed);
+  return joinedList.split("\n");
 }

628b3a6b0eaa853f38dd07b840a69c4a3f7f5855
Frank Thompson <frankthompson@gmail.com>
2026-04-24 09:25:17 -0700

    clean up doc and remove gatsby

--- app/doc/[term]/page.tsx
+++ app/doc/[term]/page.tsx
@@ -1,77 +1,74 @@
-import { Game, ORDERED_GAMES, ORDERED_PROPER_GAMES } from "@/app/goals";
+import { Game, ORDERED_GAMES } from "@/app/goals";
 import { DOC_LINKS } from "@/app/practice/docLinks";
 import { SequenceMatcher } from "difflib";
-import { redirect, notFound } from "next/navigation";
+import { redirect } from "next/navigation";
 
 const ALIASES: { [alias: string]: Game } = {
-    "camp": "campanella",
-    "camp1": "campanella",
-    "camp2": "campanella2",
-    "camp3": "campanella3",
-    "campi": "campanella",
-    "campii": "campanella2",
-    "campiii": "campanella3",
-    "c1": "campanella",
-    "c2": "campanella2",
-    "c3": "campanella3",
-    "mortol1": "mortol",
-    "mortoli": "mortol",
-    "mortol2": "mortolii",
-    "m1": "mortol",
-    "mi": "mortol",
-    "m2": "mortolii",
-    "mii": "mortolii",
-    "pq": "pilotquest",
-    "rh": "railheist",
-    "roi": "rockonisland",
-    "lod": "lordsofdiskonia",
-    "mg": "magicgarden",
-    "pz": "planetzoldath",
-    "bk": "blockkoala",
-    "bbr": "thebigbellrace",
-    "bigbellrace": "thebigbellrace",
-    "od": "oniondelivery",
-    "carcar": "caramelcaramel",
-    "car": "caramelcaramel",
-    "ph": "partyhouse",
-    "hf": "hotfoot",
-    "hc": "hypercontender",
-    "ob": "overbold",
-    "sw": "starwaspir",
-    "mm": "miniandmax",
-    "mandm": "miniandmax",
-    "co": "cyberowls",
-    "gen": "general",
+  camp: "campanella",
+  camp1: "campanella",
+  camp2: "campanella2",
+  camp3: "campanella3",
+  campi: "campanella",
+  campii: "campanella2",
+  campiii: "campanella3",
+  c1: "campanella",
+  c2: "campanella2",
+  c3: "campanella3",
+  mortol1: "mortol",
+  mortoli: "mortol",
+  mortol2: "mortolii",
+  m1: "mortol",
+  mi: "mortol",
+  m2: "mortolii",
+  mii: "mortolii",
+  pq: "pilotquest",
+  rh: "railheist",
+  roi: "rockonisland",
+  lod: "lordsofdiskonia",
+  mg: "magicgarden",
+  pz: "planetzoldath",
+  bk: "blockkoala",
+  bbr: "thebigbellrace",
+  bigbellrace: "thebigbellrace",
+  od: "oniondelivery",
+  carcar: "caramelcaramel",
+  car: "caramelcaramel",
+  ph: "partyhouse",
+  hf: "hotfoot",
+  hc: "hypercontender",
+  ob: "overbold",
+  sw: "starwaspir",
+  mm: "miniandmax",
+  mandm: "miniandmax",
+  co: "cyberowls",
+  gen: "general",
 };
 
 export default async function Page({
-    params,
+  params,
 }: {
-    params: Promise<{ term: string }>
+  params: Promise<{ term: string }>;
 }) {
-    const { term } = await params;
+  const { term } = await params;
 
-    if (term === "gatsby") {
-        return redirect("https://blog.prepscholar.com/the-great-gatsby-dr-tj-eckleburg-eyes-symbol");
+  let bestScore = null;
+  let bestGame = null;
+
+  for (const game of ORDERED_GAMES) {
+    const score = new SequenceMatcher(null, game, term).ratio();
+    if (bestScore == null || score > bestScore) {
+      bestScore = score;
+      bestGame = game;
     }
-    let bestScore = null;
-    let bestGame = null;
+  }
 
-    for (const game of ORDERED_GAMES) {
-        const score = new SequenceMatcher(null, game, term).ratio();
-        if (bestScore == null || score > bestScore) {
-            bestScore = score;
-            bestGame = game;
-        }
+  for (const alias of Object.keys(ALIASES)) {
+    const score = new SequenceMatcher(null, alias, term).ratio();
+    if (bestScore == null || score > bestScore) {
+      bestScore = score;
+      bestGame = ALIASES[alias];
     }
+  }
 
-    for (const alias of Object.keys(ALIASES)) {
-        const score = new SequenceMatcher(null, alias, term).ratio();
-        if (bestScore == null || score > bestScore) {
-            bestScore = score;
-            bestGame = ALIASES[alias];
-        }
-    }
-
-    redirect(DOC_LINKS[bestGame!]);
+  redirect(DOC_LINKS[bestGame!]);
 }

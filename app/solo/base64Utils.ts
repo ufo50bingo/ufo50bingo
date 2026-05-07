628a757d0d53f2aad2eddc459d094cbdc9d18a7d
Frank Thompson <frankthompson@gmail.com>
2026-05-06 22:04:21 -0700

    partial brotli

--- app/solo/base64Utils.ts
+++ app/solo/base64Utils.ts
@@ -1,3 +1,5 @@
+import brotliPromise from "brotli-wasm"; // Import the default export
+
 function base64ToBytes(base64: string): Uint8Array<ArrayBuffer> {
   const binString = atob(base64);
   return Uint8Array.from(binString, (v, _) => v.codePointAt(0)!);
@@ -14,6 +16,11 @@ function base64UrlEncode(str: string): string {
   return bytesToBase64(new TextEncoder().encode(str))
     .replace(/\//g, "_")
     .replace(/\+/g, "-");
+}
+
+async function compress(str: string): Promise<string> {
+  const brotli = await brotliPromise;
+  return brotli.compress(new TextEncoder().encode(str));
 }
 
 function base64UrlDecode(str: string): string {
